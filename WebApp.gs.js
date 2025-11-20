0dc25c010af3235067009a31bdb802f6635b1936
Gabrieljgb18 <ggomezb@fi.uba.ar>
2025-11-19 23:32:56 -0300

    Inline partial includes to avoid template errors

--- WebApp.gs.js
+++ WebApp.gs.js
@@ -1,12 +1,5 @@
 function doGet() {
-  const output = HtmlService
-    .createTemplateFromFile('FrontedErp')
-    .evaluate(); // permite usar includes de parciales
-
-  return output.addMetaTag('viewport', 'width=device-width, initial-scale=1');
-}
-
-// Helper para incluir parciales desde las plantillas
-function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename).getContent();
+  return HtmlService
+    .createHtmlOutputFromFile('FrontedErp')
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1');
 }
